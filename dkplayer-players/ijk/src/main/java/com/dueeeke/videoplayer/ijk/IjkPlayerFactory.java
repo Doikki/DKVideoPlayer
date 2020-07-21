f73931c5dfec237c720ac004c3d405e52a642e6a
susyimes <705710652@qq.com>
2020-07-21 12:25:55 +0800

    remove test

--- dkplayer-players/ijk/src/main/java/com/dueeeke/videoplayer/ijk/IjkPlayerFactory.java
+++ dkplayer-players/ijk/src/main/java/com/dueeeke/videoplayer/ijk/IjkPlayerFactory.java
@@ -14,9 +14,6 @@ public class IjkPlayerFactory extends PlayerFactory<IjkPlayer> {
 
     @Override
     public IjkPlayer createPlayer(Context context) {
-        IjkPlayer ijkPlayer = new IjkPlayer(context);
-        ijkPlayer.getMediaPlayer().setOption(IjkMediaPlayer.OPT_CATEGORY_PLAYER,"max-buffer-size",50000L);
-
-        return ijkPlayer;
+        return new IjkPlayer(context);
     }
 }
